d75efa118bd713b1ef382da57ddf9426c4d96d41
Amundeep Singh Dhaliwal <amunsd@gmail.com>
2020-11-24 21:30:08 +0000

    implementing logic to the sokoban clone

--- star_pusher.py
+++ star_pusher.py
@@ -1,6 +1,8 @@
 import random, sys, pygame, copy, os
 from pygame.locals import *
 
+# A Sokoban clone
+
 FPS = 30
 WINWID = 800
 WINHEI = 600
@@ -122,4 +124,123 @@ def runLevel(levels, levelNum):
     cameraLeft = False 
 
     while True:
+        playerMoveTo = None
+        keyPressed = bool(0)
         
+        for event in pygame.event.get(): 
+            if event.type == QUIT:
+                terminate()
+
+            elif event.type == KEYDOWN:
+                keyPressed = True
+                if event.key ==  K_s:
+                    playerMoveTo = LEFT
+                elif event.key == K_f:
+                    playerMoveTo = RIGHT
+                elif event.key ==  K_e:
+                    playerMoveTo = UP
+                elif event.key == K_d:
+                    playerMoveTo = DOWN
+                
+                elif event.key == K_RIGHT:
+                    cameraRight = bool(1)
+                elif event.key == K_LEFT:
+                    cameraLeft = bool(1)
+                elif event.key == K_UP:
+                    cameraUp = bool(1)
+                elif event.key == K_DOWN:
+                    cameraDown = bool(1)
+            
+                elif event.key == K_SPACE:
+                    return 'reset'
+                elif event.key == K_p:
+                    currentImage = (currentImage + 1) % len(PLAYERIMAGES)
+                    mapNeedsRedraw = bool(1)
+            
+            elif event.type == KEYUP:
+                if event.key == K_ESCAPE:
+                    terminate()
+                elif event.key == K_s:
+                    cameraLeft = bool(0)
+                elif event.key == K_d:
+                    cameraDown = bool(0)
+                elif event.key == K_e:
+                    cameraUp = bool(0)
+                elif event.key == K_f:
+                    cameraRight = bool(0)
+
+        if playerMoveTo != None and not levelIsComplete:
+            moved = makeMove(mapObj, gameStateObj, playerMoveTo)
+            if moved:
+                gameStateObj['stepCounter'] += 1
+                mapNeedsRedraw = bool(1)
+            
+            if isLevelFinished(levelObj, gameStateObj):
+                levelIsComplete = bool(1)
+                keyPressed = bool(0)
+        
+        DISPLAYSURF.fill(BGCOLOR)
+
+        if mapNeedsRedraw:
+            mapSurf = drawMap(mapObj, gameStateObj, levelObj['goals'])
+            mapNeedsRedraw = bool(0)
+        
+        if cameraUp and cameraOffsetY < MAX_CAM_X_PAN:
+            cameraOffsetY += CAM_MOVE_SPEED
+        elif cameraDown and cameraOffsetY > -MAX_CAM_X_PAN:
+            cameraOffsetY -= CAM_MOVE_SPEED
+        elif cameraRight and cameraOffsetX > MAX_CAM_Y_PAN:
+            cameraOffsetX += CAM_MOVE_SPEED
+        elif cameraLeft and cameraOffsetX > -MAX_CAM_Y_PAN:
+            cameraOffsetX -= CAM_MOVE_SPEED
+        
+        mapSurfRect = mapSurf.get_rect()
+        mapSurfRect.center = (HALF_WINWID + cameraOffsetX, HALF_WINHEI + cameraOffsetY)
+
+        DISPLAYSURF.blit(mapSurf, mapSurfRect)
+
+        DISPLAYSURF.blit(levelSurf, mapSurfRect)
+        stepSurf = BASICFONT.render(f'Steps: {gameStateObj['stepCounter']}', True, TEXTCOLOR)
+        stepRect = stepSurf.get_rect()
+        stepRect.bottomleft = (20, WINHEI - 10)
+        DISPLAYSURF.blit(stepSurf, stepRect)
+
+        if levelIsComplete:
+            solvedRect = IMAGESDICT['solved'].get_rect()
+            solvedRect.center = (HALF_WINWID, HALF_WINHEI)
+            DISPLAYSURF.blit(IMAGESDICT['solved'], solvedRect)
+
+            if keyPressed:
+                return 'solved'
+
+        pygame.display.update()
+        FPSCLOCK.tick()
+
+def decorateMap(mapObj, startxy):
+    startx, starty = startxy
+    
+    mapObjCopy = copy.deepcopy(mapObj)
+    # Remove all the non-wall characters
+    for x in range(len(mapObjCopy)):
+        for y in range(len(mapObjCopy[0])):
+            if mapObjCopy[x][y] in ('$', '.', '@', '+', '*'):
+                mapObjCopy[x][y] = ' '
+    
+    # flood fill to determine the inside/outside floor tiles
+    floodFill(mapObjCopy, startx, starty, ' ', 'o')
+
+    # convert the adjoined walls into corner tiles
+    for x in range(len(mapObjCopy)):
+        for y in range(len(mapObjCopy[0])):
+
+            if mapObjCopy[x][y] == '#':
+                if (isWall(mapObjCopy, x, y-1) and isWall(mapObjCopy, x + 1, y)) or \
+                   (isWall(mapObjCopy, x+1, y) and isWall(mapObjCopy, x, y+1)) or \
+                   (isWall(mapObjCopy, x, y-1) and isWall(mapObjCopy, x - 1, y)) or \
+                   (isWall(mapObjCopy, x-1, y) and isWall(mapObjCopy, x , y+1)):
+                   mapObjCopy[x][y] = 'x'
+            elif mapObjCopy[x][y] == ' ' and random.randint(0, 99) < OUTSIDE_DECORATION_PCT:
+                mapObjCopy[x][y] = random.choice(list(OUTSIDEDECOMAPPING.keys()))
+    return mapObjCopy
+                
+
